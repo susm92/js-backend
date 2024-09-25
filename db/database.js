bfa1c614eadfd297c2b1a20cac62f1dab62716cd
susm92 <sucrosmrkovic92@gmail.com>
2024-09-25 11:58:43 +0200

    fixed eslinter and deployed backend to azure

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const mongo = require("mongodb").MongoClient;
 
 const database = {
     getDb: async function getDb(dbName, collectionName) {
-        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.vqesk.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+        let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.vqesk.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;    // eslint-disable-line
 
         const client  = await mongo.connect(dsn, {
             useNewUrlParser: true,
