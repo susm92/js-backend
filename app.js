d0bf6997b3bf2b6eac0a74f2e64aa906c496b69e
susm92 <sucrosmrkovic92@gmail.com>
2024-10-17 11:54:52 +0200

    added sockets feature to backend

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const cors = require('cors');
-//const morgan = require('morgan');
+const httpServer = require("http").createServer(app);
 
 require('./config/passport');
 require('dotenv').config();
@@ -13,6 +13,7 @@ const authRoutes = require('./routes/auth.js');
 const mgRoutes = require('./routes/mailgun.js');
 
 const port = process.env.PORT || 8080;
+const socketPORT = process.env.SOCKET_PORT || 3030;
 
 app.use(cors());
 app.options('*', cors());
@@ -20,6 +21,39 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
+
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected', socket.id);
+
+    // Handle joining a room
+    socket.on('joinRoom', (documentId) => {
+        socket.join(documentId); // Join the specified room
+    });
+
+    // Listening for content changes from a user
+    socket.on('contentChange', ({ documentId, newContent }) => {
+        console.log('Content change:', newContent);
+        // Broadcast the updated content to other users in the same document room
+        socket.to(documentId).emit('updateContent', newContent);
+    });
+
+  // Listening for title changes from a user
+    socket.on('titleChange', ({ documentId, newTitle }) => {
+        console.log('Title change:', newTitle);
+        // Broadcast the updated title to other users in the room
+        socket.to(documentId).emit('updateTitle', newTitle);
+  });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected', socket.id);
+    });
+});
 
 // Add a route
 app.get("/", (req, res) => {
@@ -38,3 +72,6 @@ app.use('/mailgun', mgRoutes);
 
 // Start up server
 app.listen(port, () => console.log(`Example API listening on port ${port}!`));
+
+io.listen(socketPORT);
+console.log(`Socket.IO server is running on port ${socketPORT}`);
